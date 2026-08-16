ab62033a75617ae6bb9f3c68021189529a923197
YliaCallan <traffictorch@gmail.com>
2026-08-16 10:20:33 +1000

    Disable PWA Caching

--- sw.js
+++ sw.js
@@ -1,67 +1,23 @@
-// sw.js
-const CACHE_NAME = 'prod-bond-model-v1';
-const urlsToCache = [
-  '/productivity-bond-model/index.html',
-  '/productivity-bond-model/manifest.json',
-  '/productivity-bond-model/favicon.ico'
-];
+// sw.js – Caching completely disabled
 
-// Install
 self.addEventListener('install', event => {
+  // Skip waiting to activate immediately
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Activate
-self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
+    // Delete any existing caches
     caches.keys().then(cacheNames => {
       return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheWhitelist.indexOf(cacheName) === -1) {
-            return caches.delete(cacheName);
-          }
-        })
+        cacheNames.map(cacheName => caches.delete(cacheName))
       );
     })
   );
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        // Cache hit
-        if (response) {
-          return response;
-        }
-
-        // Clone the request (streams can only be consumed once)
-        const fetchRequest = event.request.clone();
-
-        return fetch(fetchRequest).then(response => {
-          // Only cache valid basic responses
-          if (!response || response.status !== 200 || response.type !== 'basic') {
-            return response;
-          }
-
-          // Clone the response (streams can only be consumed once)
-          const responseToCache = response.clone();
-
-          caches.open(CACHE_NAME)
-            .then(cache => {
-              cache.put(event.request, responseToCache);
-            });
-
-          return response;
-        });
-      })
+self.addEventListener('activate', event => {
+  // Take control of all pages without reload
+  event.waitUntil(
+    clients.claim()
   );
 });
+
+// No 'fetch' listener = no caching, all requests go direct to network.
